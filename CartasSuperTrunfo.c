fb980db1e6687772e5900d21670684ef78b73038
motta7077-dev <motta7077@gmail.com>
2026-04-14 01:19:14 +0000

    desafio nivel novato rio de janeiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,26 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado ='b';
+  char codigo[]='B';
+  char nomecidade[]="Riojaneiro";
+  int populacao = 6748000;
+  float area = 1200.25;
+  float pib = 300.50;
+  int pontosTuristicos = 30;
 
-  // Área para entrada de dados
+printf("---dados da cidade---\n");
+printf("estado: %c\n", estado);
+printf("Codigo: %s\n",codigo);
+printf("Nome da cidade: %s\n",nomecidade);
+printf("populacao:%d\n",populacao);
+printf("Area:%.2f km2,\n", area);
+printf("pib:%2f bilhoes de reias\n",pib);
+printf("pontos turisticos:%d\n", pontosTuristicos);
+  // Área para entda de dados
 
   // Área para exibição dos dados da cidade
 
 return 0;
+
 } 
